e978b18b032f394f081efc95b0418e7e6b725a51
Erik Kastner <kastner@gmail.com>
2008-05-04 21:23:20 -0400

    automatic upload notification / redirect. caching the file-list from s3

--- mp3.rb
+++ mp3.rb
@@ -31,11 +31,19 @@ before do
 end
 
 get "/" do
-  @files = Bucket.find(BUCKET).objects.collect do |object|
-    "<a href='http://#{BUCKET}/#{object.key.to_s}'>#{URI.unescape(object.metadata[:name].to_s)}</a>" 
-  end.join("<br/>")
+  @files = CACHE["file-list"] ||= begin
+    puts "Cache miss on file-list"
+    Bucket.find(BUCKET).objects.collect do |object|
+      "<a class='mp3' id='#{object.key.to_s}' href='http://#{BUCKET}/#{object.key.to_s}' >#{URI.unescape(object.metadata[:name].to_s)}</a>" 
+    end.join("<br/>")
+  end
   
   erb :index
+end
+
+get "/check" do
+  key = params[:key]
+  CACHE["download-#{key}"] ||= 0
 end
 
 get "/fetch" do
@@ -48,34 +56,38 @@ get "/fetch" do
   end
   
   remote_header = %x{curl -s -u #{ENV["EZNEWS_USER"]}:#{ENV["EZNEWS_PASS"]} --head "#{url}"}
-    
+  
+  CACHE["download-#{name}"] = "0"
   Thread.new do
     puts "In thread"
     bits = open(CACHE[params[:key]], :http_basic_authentication => [ENV["EZNEWS_USER"], ENV["EZNEWS_PASS"]])
     
     item = S3Object.store(name, bits, BUCKET, :access => :public_read, :content_type => remote_header[/Content-Type: ([^\n]*)/, 1], "x-amz-meta-name" => File.basename(url))
     puts "uploaded!"
+    CACHE["download-#{name}"] = "1"
+    CACHE.delete("file-list")
   end
   
-  redirect "/?send=true"
+  redirect "/?send=true&key=#{name}"
 end
 
 get "/search" do
   require 'hpricot'
   contents = eznews_contents(params[:q])
   p = Hpricot.XML(contents)
-  @files = (p / "item link").map do |l| 
-    link = l.innerHTML
+  @files = (p / "item").map do |i|
+    link = i.at("link").innerHTML
+    size = i.at("description").innerHTML[/\d+[\.\d]*\s(MB|K)/]
     string = URI.unescape(File.basename(link))
     hash_key = SHA1.hexdigest(link)
     CACHE[hash_key] = link
-    "<a href='/fetch?key=#{hash_key}'>#{string}</a>" 
+    "<a href='/fetch?key=#{hash_key}'>#{string}</a> [#{size}]"
   end.reverse.join "<br/>"
   
   erb :results
 end
 
 def eznews_contents(terms)
-  url = "http://members.easynews.com/global4/search.html?gps=#{URI.escape(terms)}&fex=mp3&fty%5B%5D=AUDIO&s1=dsize&s1d=-&s2=dtime&s2d=-&s3=dsize&s3d=%2B&pby=20&pno=1&sS=5"
+  url = "http://members.easynews.com/global4/search.html?gps=#{URI.escape(terms)}&fex=mp3&fty%5B%5D=AUDIO&s1=dtime&s1d=-&s2=dsize&s2d=-&s3=dsize&s3d=%2B&pby=20&pno=1&sS=5"
   open(url, :http_basic_authentication => [ENV["EZNEWS_USER"], ENV["EZNEWS_PASS"]]).read
 end
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,4 +3,21 @@ $(function() {
   $("#search").submit(function() {
     $("#spinner").show();
   });
+  if ($("#notice").length > 0) {
+    var key = $("#notice").attr("class");
+    var interval = setInterval(function() {
+      $.get("/check?key=" + key, function(data) {
+        if (data == 1) {
+          clearInterval(interval);
+          $("#notice").html("All done... Redirecting");
+          setTimeout(function() { window.location = "/#" + key; }, 1000);
+        }
+      });
+    }, 10000);
+  }
+  if (location.hash) {
+/*    $(location.hash).addClass("highlight");//.animate("font-size", "200%");*/
+/*    alert($(location.hash).length);*/
+    $(location.hash).animate({fontSize: "150%"}, 1500);
+  }
 });
